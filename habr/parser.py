c631711d280072f02ead25d8880d1fd3dae2c18e
MaximBurtsev <zadrot-lol@list.ru>
2022-07-28 13:15:47 +0300

    create new app (habr) and add parser for it

---
+++ habr/parser.py
@@ -0,0 +1,67 @@
+from typing import NamedTuple
+from enum import Enum
+
+import requests
+from bs4 import BeautifulSoup
+import fake_useragent
+
+
+class URL(Enum):
+    ALL_FLOWS = "https://habr.com/ru/all/"
+    WITH_RATING = "https://habr.com/ru/all/top10/"
+    TOP_WEEKLY = "https://habr.com/ru/top/weekly/"
+
+
+class PostTuple(NamedTuple):
+    url: str
+    votes: str
+    views: str
+
+
+class Parser:
+    def __init__(self) -> None:
+
+        self.headers = {"user-agent": fake_useragent.UserAgent().random}
+
+    def _get_html_page(self, url: str) -> str:
+
+        response = requests.get(url, headers=self.headers)
+
+        if response.status_code == 200:
+            return response.text
+        else:
+            raise Exception("Parsing error")
+
+    def _get_raw_posts_from_page(self, page) -> list[str]:
+
+        soup = BeautifulSoup(page, "lxml")
+
+        posts_block = soup.find_all("div", {"class": "tm-articles-list"})[0]
+        raw_posts_list = posts_block.find_all(
+            "article", {"class": "tm-articles-list__item"}
+        )
+
+        return raw_posts_list
+
+    def _get_clean_posts(self, raw_posts) -> list[PostTuple]:
+
+        clean_posts = []
+
+        for post in raw_posts:
+            url = "https://habr.com" + post.find(
+                "a", {"class": "tm-article-snippet__title-link"}
+            ).get("href")
+            votes = post.find("span", {"data-test-id": "votes-meter-value"}).text
+            views = post.find("span", {"class": "tm-icon-counter__value"}).text
+
+            clean_posts.append(PostTuple(url, votes, views))
+
+        return clean_posts
+
+    def get_posts(self, url) -> list[PostTuple]:
+
+        page = self._get_html_page(url=url)
+        raw_posts = self._get_raw_posts_from_page(page)
+        clean_posts = self._get_clean_posts(raw_posts)
+
+        return clean_posts
