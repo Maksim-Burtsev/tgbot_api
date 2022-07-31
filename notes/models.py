507407c7dc4994691b056c21d4b381f4cb4325a5
MaximBurtsev <zadrot-lol@list.ru>
2022-07-31 16:22:33 +0300

    update notes model

--- notes/models.py
+++ notes/models.py
@@ -1,13 +1,13 @@
-from django.db import models
 from django.utils import timezone
+from django.db import models
 
 
 class Note(models.Model):
 
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    date = models.DateTimeField(default=timezone.now())
-    category = models.CharField(max_length=255)
+    date = models.DateTimeField(default=timezone.now)
+    category = models.CharField(max_length=255, null=True, blank=True)
 
     def save(self, *args, **kwargs):
         return super().save(*args, **kwargs)
