e58fd75816d97232cc7d9c3a8ee311594d993276
MaximBurtsev <zadrot-lol@list.ru>
2022-08-09 23:20:56 +0300

    feat: logged the app

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -24,7 +24,13 @@ from services import (
 
 load_dotenv()
 
-logger.add("logs/debug.log", format="{time} {level} {name} {message}", level="DEBUG", rotation="5 MB")
+logger.add(
+    "logs/debug.log",
+    format="{time} {level} {name} {message}",
+    level="DEBUG",
+    rotation="08:00",
+    compression="zip",
+)
 
 MY_ID = int(os.getenv("MY_ID"))
 TOKEN = os.getenv("BOT_TOKEN")
@@ -38,7 +44,9 @@ def start(message):
     item2 = types.KeyboardButton("daily posts")
     markup.add(item1)
     markup.add(item2)
-    bot.send_message(message.chat.id, "hi", reply_markup=markup)
+
+    logger.debug(f"/start by {message.chat.id}")
+    return bot.send_message(message.chat.id, "hi", reply_markup=markup)
 
 
 @bot.message_handler(commands=["help"])
@@ -58,7 +66,8 @@ def help_(message):
         /del_note (pk)
         /get_notes [category | name | None]
     """
-    bot.send_message(chat_id=message.chat.id, text=text)
+    logger.debug(f"/help by {message.chat.id}")
+    return bot.send_message(chat_id=message.chat.id, text=text)
 
 
 @bot.message_handler(content_types="text")
@@ -68,36 +77,53 @@ def main(message):
     text = message.text
 
     if chat_id != MY_ID:
+        logger.debug(f"403 for {chat_id}")
         return bot.send_message(chat_id, "forbidden")
 
     if text == ("daily posts"):
         posts = get_habr_posts()
+
+        logger.debug("send daily posts")
         return send_posts(bot, message, posts)
 
     elif text.startswith("/week") and len(text) < 7:
         posts = get_habr_posts(category="top_weekly")
+
+        logger.debug("send best of week posts")
         return send_posts(bot, message, posts)
 
     elif text == "/rating" and len(text) < 10:
         posts = get_habr_posts(category="with_rating")
+
+        logger.debug("send posts with rating")
         return send_posts(bot, message, posts)
 
     elif text.startswith("/daily_purchases"):
         date_today = str(datetime.datetime.now().date())
         purchases_list = get_purchases_report(from_date=date_today)
+
+        logger.debug("get daily purchases")
+
         return send_purchases(bot, message, purchases_list)
 
     elif text.startswith("monthly costs"):
         first_day, last_day = get_current_month_dates()
         purchases_list = get_purchases_report(first_day, last_day)
+
+        logger.debug("send monthly purchases")
+
         return send_purchases(bot, message, purchases_list)
 
     elif text.startswith("/del_purchase") and len(text.split()) == 2:
 
-        res = remove_purchase(name=text.split()[1])
+        name = text.split()[1]
+        res = remove_purchase(name)
+
         if res:
+            logger.debug(f"remove purchase name={name}")
             return bot.send_message(chat_id, "success")
         else:
+            logger.debug(f"remove purchases (404) name={name}")
             return bot.send_message(chat_id, "404")
 
     elif text.startswith("/month_purchases") and 2 <= len(text.split()) <= 3:
@@ -109,10 +135,13 @@ def main(message):
 
         res = get_monthly_costs(month, year)
 
+        logger.debug(f"get monthly costs: month={month}, year={year}")
         return bot.send_message(chat_id, res)
 
     elif text == "/note":
         text = """structure of message:\n\n /node (category | None)\n name\n description | None"""
+
+        logger.debug("send /note description")
         return bot.send_message(chat_id, text)
 
     elif text.startswith("/note") and len(text.split("\n")) >= 2:
@@ -125,18 +154,29 @@ def main(message):
         res = create_note(name, category, description)
 
         response_text = "Success" if res else "Something wrong..."
-        bot.send_message(chat_id, response_text)
+
+        logger.debug(
+            f"create note name={name}, category={category}, desc={description} with result: {response_text}"
+        )
+        return bot.send_message(chat_id, response_text)
 
     elif text.startswith("/del_note") and len(text.split()) == 2:
-        res = delete_notes(text.split()[1])
+        query = text.split()[1]
+        res = delete_notes(query)
         response_text = "Succes" if res else "Something wrong..."
+
+        logger.debug(f"/del_note with query={query}, res: {response_text}")
         return bot.send_message(chat_id, response_text)
 
     elif text.startswith("/get_notes") and len(text.split()) == 2:
-        notes = get_notes(text.split()[1])
+        query = text.split()[1]
+        notes = get_notes(query)
+
         if notes:
             for note in notes:
                 bot.send_message(chat_id, note)
+
+        logger.debug(f"/get_notes with query={query}")
 
     elif len(text.split()) % 2 == 0:
         raw_purchases = text.split()
@@ -144,9 +184,15 @@ def main(message):
         if all([price.isdigit() for price in raw_purchases[1::2]]):
             res = create_purchases(raw_purchases)
             response_text = "Succes" if res else "Opppppsss..."
+
+            logger.debug(f"add purchases {raw_purchases}, result: {response_text}")
             return bot.send_message(chat_id, response_text)
 
+        logger.debug(f"add invalid purchase {raw_purchases}")
         return bot.send_message(chat_id, "cost must be digit")
+
+    logger.debug("unknown command {text}")
+
 
 if __name__ == "__main__":
     bot.infinity_polling()
